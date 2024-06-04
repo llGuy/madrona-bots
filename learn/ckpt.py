0acb470a66b8defcc0e7691a8fa82e1c54d29096
Vivek Vajipey <vvajipey@cocoflops2.stanford.edu>
2024-06-03 22:44:25 -0700

    Fixed universe creation and wandb logging

--- learn/ckpt.py
+++ learn/ckpt.py
@@ -17,6 +17,7 @@ class CheckpointManager:
         # Determine the filename based on whether it's the latest or a best metric model
         if metric_name == "latest":
             filename = f'latest_model_epoch_{epoch}.pt'
+            self._delete_old_files(full_path, f'latest_model_epoch_*.pt')
         else:
             filename = f'best_{metric_name}_epoch_{epoch}.pt'
             # Delete any existing best model files for the same metric
@@ -34,30 +35,26 @@ class CheckpointManager:
         except Exception as e:
             print(f"Failed to save model due to error: {e}")
 
-    def load(self, model_class, optimizer_class, sub_dir, init_fn, metric_name="latest", epoch=None):
-        if metric_name == "latest":
-            assert epoch is not None, "Epoch must be specified for loading the latest model"
-            load_path = os.path.join(self.base_ckpt_dir, sub_dir, f'latest_model_epoch_{epoch}.pt')
-        else:
-            # Load the best model for the specified metric
-            load_path = os.path.join(self.base_ckpt_dir, sub_dir, f'best_{metric_name}_epoch_*.pt')
-            files = glob.glob(load_path)
-            if not files:
-                raise FileNotFoundError(f"No best model found for metric {metric_name}")
-            # Assuming the latest best model is needed
-            files.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]), reverse=True)
-            load_path = files[0]
-        
+    def load(self, model_class, optimizer_class, sub_dir, init_fn, metric_name="latest"):
+        pattern = f'latest_model_epoch_*.pt' if metric_name == "latest" else f'best_{metric_name}_epoch_*.pt'
+        load_path = os.path.join(self.base_ckpt_dir, sub_dir, pattern)
+        files = glob.glob(load_path)
+        if not files:
+            raise FileNotFoundError(f"No model found for metric {metric_name}")
+
+        # Select the file with the highest epoch number
+        files.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]), reverse=True)
+        load_path = os.path.join(self.base_ckpt_dir, sub_dir, files[0])
+
+        assert os.path.isfile(load_path), f"Model file not found at {load_path}"
+        print(f"Loading model from {load_path}")
+
         if self.restore and os.path.isfile(load_path):
             checkpoint = torch.load(load_path)
             model = init_fn(checkpoint['model_config']) if init_fn else model_class(checkpoint['model_config'])
             model.load_state_dict(checkpoint['model_state_dict'])
             optimizer = optimizer_class(model.parameters())
             optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            return model, optimizer
-        else:
-            model = init_fn() if init_fn else model_class()
-            optimizer = optimizer_class(model.parameters(), lr=3e-4)
             return model, optimizer
 
     def _check_dir(self, path):
